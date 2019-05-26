ca3326617949ce26f65901a20e29cfcf4c62f84d
Woo Jin Lee <quikhearst@kaist.ac.kr>
2019-05-25 22:01:20 -0700

    team6.java restore

--- src/main/java/Team6.java
+++ src/main/java/Team6.java
@@ -7,32 +7,15 @@ import org.jsoup.select.Elements;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
-import java.util.logging.Level;
-
-import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.WebConsole.Logger;
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
-import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlOption;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
-import com.gargoylesoftware.htmlunit.html.HtmlSelect;
-import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
-import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 
 // TODO: create new file/class for each function to organize the whole project
 public class Team6 {
 	
     public static void main(String[] args) throws IOException {
     	
-    	java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(Level.OFF);
-    	
         // Reference for events: https://www.w3schools.com/tags/ref_eventattributes.asp
     	
     	
-    	
-    	/*
         final Collection<String> EVENTS = Arrays.asList(
                 "onchange","onfocus","onselect","onsubmit",
                 "onclick", "onmouseover", "ondbclick", "onwheel",
@@ -108,27 +91,6 @@ public class Team6 {
         // TODO: connecting again takes some time, do we have any other way?
         Document doc2 = Jsoup.connect(linkHref).get();
         System.out.println(doc2.title());
-        */
-    	
-    	/*
-    	final WebClient webClient = new WebClient();
-    	HtmlPage page3;
-    	page3 = webClient.getPage("https://melodize.github.io/");
-    	HtmlForm loginForm = page3.getFormByName("Sign In");
-    	HtmlTextInput username = loginForm.getInputByName("NameofUsernameElement");
-    	HtmlPasswordInput pass = loginForm.getInputByName("NameofPassowordElement");
-    	HtmlSubmitInput b = loginForm.getInputByValue("LoginButtonValue");
-
-    	username.setValueAttribute("Actualy Username");
-    	pass.setValueAttribute("Actual Password");
-    	HtmlPage page2;
-    	page2 = b.click();
-    	*/
-    	
-    	final WebClient webClient = new WebClient(BrowserVersion.CHROME);
-    	final HtmlPage page = webClient.getPage("http://htmlunit.sourceforge.net");
-    	System.out.println("HtmlUnit - Welcome to HtmlUnit".contentEquals(page.getTitleText()));
-
-    	
+        
     }
 }
